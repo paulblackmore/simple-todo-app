5ceef3dbd1fcef59f2965d4fa2e3a19bfbaaae80
paul <paulmichaelblackmore@gmail.com>
2025-02-06 17:06:32 +0000

    feat: add dynamic css when todo input checked

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,7 @@
 import styles from './TodoItem.module.css';
 export const TodoItem = () => {
   return (
-    <div className={styles.root}>
+    <div className={styles.root} data-selected={true}>
       <span>Get food</span>
       <input type='checkbox' />
     </div>
