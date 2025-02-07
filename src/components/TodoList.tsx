e9f1cb11a24499761846c8c6254913e4303ce99b
paul <paulmichaelblackmore@gmail.com>
2025-02-07 14:34:25 +0000

    feat: add fallback UI for loading and error states

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,19 @@
 import { TodoItem } from './TodoItem';
 import styles from './TodoList.module.css';
 export const TodoList = () => {
-  return (
+  const isLoading = false;
+  const isError = false;
+
+  return isLoading ? (
+    <div className={styles['fallback-layout']}>
+      <h2>Loading...</h2>
+    </div>
+  ) : isError ? (
+    <div className={styles['fallback-layout']}>
+      <h2>We're sorry!</h2>
+      <span>We have experienced a issue fetching your Todos</span>
+    </div>
+  ) : (
     <article className={styles.root}>
       <TodoItem />
       <TodoItem />
