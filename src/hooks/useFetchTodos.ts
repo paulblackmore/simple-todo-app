00de91b663b6fb130722808311a818037fd41f6a
paul <paulmichaelblackmore@gmail.com>
2025-02-07 15:47:25 +0000

    chore: create .env file and move API url

--- src/hooks/useFetchTodos.ts
+++ src/hooks/useFetchTodos.ts
@@ -3,7 +3,7 @@ import { Todo } from '../types/Todo';
 
 const fetchTodos = async () => {
   try {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/todos`);
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/todos`);
     if (!response.ok) throw new Error('Network response was not ok');
 
     return response.json();
