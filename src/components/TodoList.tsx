d31475a925b1bd5a5a6cb2625fa41bf3bcceaac0
paul <paulmichaelblackmore@gmail.com>
2025-02-08 20:56:12 +0000

    refactor: create re-usable component for fallback states

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,25 +2,31 @@ import { TodoItem } from './TodoItem';
 import { useFetchTodos } from '../hooks/useFetchTodos';
 import { Todo } from '../types';
 import styles from './TodoList.module.css';
+import { PropsWithChildren } from 'react';
+
+const FallbackLayout = ({ children }: PropsWithChildren) => (
+  <div className={styles['fallback-layout']}>{children}</div>
+);
 
 export const TodoList = () => {
   const { isLoading, isError, data } = useFetchTodos();
   const isEmpty = !isLoading && !data?.length;
 
   return isLoading ? (
-    <div className={styles['fallback-layout']}>
-      <h2>Loading...</h2>
-    </div>
+    <FallbackLayout>
+      <h2>Your todos are loading...</h2>
+      <span>Please be patient, this may take some time</span>
+    </FallbackLayout>
   ) : isError ? (
-    <div className={styles['fallback-layout']}>
+    <FallbackLayout>
       <h2>We're sorry!</h2>
       <span>We have experienced a issue fetching your Todos</span>
-    </div>
+    </FallbackLayout>
   ) : isEmpty ? (
-    <div className={styles['fallback-layout']}>
+    <FallbackLayout>
       <h2>Nothing to do...</h2>
       <span>Start adding some todos above</span>
-    </div>
+    </FallbackLayout>
   ) : (
     <article className={styles.root}>
       {data?.map((todo: Todo) => (
