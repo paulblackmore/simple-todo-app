0638b1f7de31af1da4985e45223a88aae96a227c
paul <paulmichaelblackmore@gmail.com>
2025-02-08 20:39:30 +0000

    feat: add empty state to TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,6 +5,7 @@ import styles from './TodoList.module.css';
 
 export const TodoList = () => {
   const { isLoading, isError, data } = useFetchTodos();
+  const isEmpty = !isLoading && !data?.length;
 
   return isLoading ? (
     <div className={styles['fallback-layout']}>
@@ -14,6 +15,11 @@ export const TodoList = () => {
     <div className={styles['fallback-layout']}>
       <h2>We're sorry!</h2>
       <span>We have experienced a issue fetching your Todos</span>
+    </div>
+  ) : isEmpty ? (
+    <div className={styles['fallback-layout']}>
+      <h2>Nothing to do...</h2>
+      <span>Start adding some todos above</span>
     </div>
   ) : (
     <article className={styles.root}>
