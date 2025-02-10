4af3bc179e03978940617058d3d77dce0cfdd957
paul <paulmichaelblackmore@gmail.com>
2025-02-10 14:33:07 +0000

    test: add data prop to AllTheProviders

--- src/test/test.utils.tsx
+++ src/test/test.utils.tsx
@@ -1,12 +1,20 @@
 import { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { mockData } from './mockData';
+import { Todo } from '../types';
 
 const queryClient = new QueryClient();
 
-export const AllTheProviders = ({ children }: PropsWithChildren) => {
+type Props = {
+  data?: Todo[];
+};
+
+export const AllTheProviders = ({
+  children,
+  data = mockData,
+}: PropsWithChildren<Props>) => {
   // Add mock data to queryClient
-  queryClient.setQueryData(['todos'], mockData);
+  queryClient.setQueryData(['todos'], data);
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
